22f38eb7d6521f7e3ae3512c5376831a1ea2d926
twilm <twilm4@tutamail.com>
2022-06-03 14:18:58 +1000

    got the selenium driver working, need to figure out how to go through <span> tags

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#options = webdriver.ChromeOptions()
+#options.add_argument('--ignore-certificate-errors')
+#options.add_argument('--incognito')
+#options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 i = "test"
 driver.get(f"https://thepiratebay.org/search.php?q={i}")
+driver.find_element(By.ID, "st")
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+table = soup.find('section', class_='col-center')
+for listing in soup.find_all('li'):
+    for items in listing:
+        print(items.get_text())
+#   for name in listing.find():
+#       print(name)
+
+
+
+#print(torrent_li)
+#driver.quit()
 
 
 
